8ea4ce687bfbce721a83d559477316bb892cf348
maskeynihal <26411488+maskeynihal@users.noreply.github.com>
2023-03-24 14:03:55 +0545

    Add index.html to testing in frontend

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const app = require("express")();
+const cors = require("cors");
+const fs = require("fs");
+
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => res.send("hello!"));
 
 app.get("/stream", (req, res) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+  );
   res.setHeader("Content-Type", "text/event-stream");
   send(res);
 });
@@ -18,6 +26,12 @@ function send(res) {
 
   setTimeout(() => send(res), 1000);
 }
+
+app.get("/test", (req, res) => {
+  res.setHeader("content-type", "text/html");
+
+  return res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(port);
 console.log(`Listening on ${port}`);
